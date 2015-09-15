210e4471e588c15a178c80a12349cc2e0d07f22e
Miguel Ramirez Jr <derock8888@gmail.com>
2015-09-15 15:01:09 -0400

    first-commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*
+  (url: String, callback: Function) -> undefined
+
+  Execute a callback function with the JSON results from the url specified.
+
+  Examples
+      var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=tacos&includes=Images,Shop";
+
+      fetchJSONP(url, function(data) {
+        // do something with data
+      });
+
+      // OR
+
+      function logData(data) {
+        console.log(data);
+      }
+
+      fetchJSONP(url, logData);
+*/
+var etsyUrl = 'https://api.etsy.com/v2/listings/active.js?api_key=uze6mb6hwotb2gzkjjibfuus&keywords=tacos&includes=Images,Shop';
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
+
+fetchJSONP(etsyUrl, function(data) {
+    console.log(data);
+});
